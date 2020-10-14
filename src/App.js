8606f7eeac2b39a1b64dfd88c07713d51dce64c0
walidnemri <nemriwalid@hotmail.com>
2020-10-14 20:53:27 +0200

    contact state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import React from 'react';
-import Contact from './components/Contact'
 import ContactList from './components/ContactList'
 
 
 function App() {
-  const walid = {
-    name: "nemri walid",
-    image: "https://pm1.narvii.com/7070/aeaeadea89d89327a8e5f4f83d10272dc337425fr1-1200-1200v2_128.jpg",
-    online: false,
-  }
-
-  const brad = {
-    name: "brad",
-    image: "https://randomuser.me/api/portraits/med/men/75.jpg",
-    online: true,
-
-  }
-
-  const yusuke = {
-    name: "yusuke",
-    image: "https://randomuser.me/api/portraits/med/men/76.jpg",
-    online: true,
-    
-  }
   return (
     <div className="App">
       <ContactList />
